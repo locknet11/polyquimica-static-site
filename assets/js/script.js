bdb6650fd816cdd140257bd3581c82c6485b187c
murielbressan <murielvbressan@gmail.com>
2022-03-27 20:08:44 -0300

    sync

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*LOGOS */
+$(document).ready(function(){
+    $('.your-class').slick({
+      jkj: setting-value
+    });
+  });
+
+  $('.variable-width').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
